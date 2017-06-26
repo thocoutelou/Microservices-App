578710256821673315561d55a462e6d0e84c606f
charles <charles.caporali@gmail.com>
2017-06-26 16:38:18 +0200

    first implementation of a message broker

--- microServices/src/main/java/com/example/microServices/MicroServicesApplication.java
+++ microServices/src/main/java/com/example/microServices/MicroServicesApplication.java
@@ -1,12 +1,18 @@
 package com.example.microServices;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicroServicesApplication {
 
+		
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesApplication.class, args);
+		
+		
 	}
 }
