48789d8ea7323b31c06a9b1552a308c362bf84a7
charles <charles.caporali@gmail.com>
2017-06-27 11:35:38 +0200

    enable basic QoS and autoAck

--- microServices/src/main/java/com/example/microServices/Receiver.java
+++ microServices/src/main/java/com/example/microServices/Receiver.java
@@ -14,17 +14,33 @@ import com.rabbitmq.client.Envelope;
 
 public class Receiver {
 	
+	
 	private final static String QUEUE_NAME = "testing_queue";
 
+	
+	/*
+	 * Simulate some work to do.
+	 */
+	private static void doWork(String task) throws InterruptedException {
+	    for (char ch: task.toCharArray()) {
+	        if (ch == '.') Thread.sleep(15000);
+	    }
+	}  
+	
 	public static void main(String[] argv) throws IOException, TimeoutException {
 	ConnectionFactory factory = new ConnectionFactory();
 	factory.setHost("localhost");
 	Connection connection = factory.newConnection();
 	Channel channel = connection.createChannel();
 	
-	channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	
+	boolean durable = true;
+	channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
     System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 	
+    channel.basicQos(1);
+    
+    
 	Consumer consumer = new DefaultConsumer(channel){
 	
 	
@@ -32,8 +48,21 @@ public class Receiver {
 	public void handleDelivery(String consumerTag, Envelope envelope,AMQP.BasicProperties properties, byte[] body) throws IOException{
 		 	String message = new String(body, "UTF-8");
 		    System.out.println(" [x] Received '" + message + "'");
+		    
+		    try {
+		        try {
+					doWork(message);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		      } finally {
+		        System.out.println(" [x] Done");
+		      }
+		    
+		    
 			}
 		};
-		channel.basicConsume(QUEUE_NAME, true, consumer);
+		boolean autoAck = false;
+		channel.basicConsume(QUEUE_NAME, autoAck, consumer);
 	}
 }
