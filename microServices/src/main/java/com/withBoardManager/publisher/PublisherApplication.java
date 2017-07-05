947ec52e621264e196de607d446633e58288d2c3
charles <charles.caporali@gmail.com>
2017-07-05 16:32:54 +0200

    Implementation of a seperate Publisher: TerminalCall

---
+++ microServices/src/main/java/com/withBoardManager/publisher/PublisherApplication.java
@@ -0,0 +1,99 @@
+package com.withBoardManager.publisher;
+
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+import com.withBoardManager.boardManager.TestingApplication;
+
+@SpringBootApplication
+public class PublisherApplication {
+	
+	
+	
+	public static String ipServer;
+	public static String service;
+	
+	public static void setService(String service) {
+		PublisherApplication.service = service;
+	}
+
+	public static void setIpServer(String ipServer) {
+		TestingApplication.ipServer = ipServer;
+	}
+	
+	@Bean
+		public String service() {
+		return service;
+	}
+
+	@Bean
+	/* Good way to parse argument */
+	public ConnectionFactory connectionFactory() {
+		return new CachingConnectionFactory(ipServer);
+	}
+
+	@Bean
+	public AmqpAdmin amqpAdmin() {
+		return new RabbitAdmin(connectionFactory());
+	}
+	
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("sb-BoardManager-exchange");
+	}	
+
+
+	public static void main(String[] args) {
+		
+		
+		Options options = new Options();
+
+		Option ipServer = new Option("i", "ipServer", true, "Ip of the RabbitMQ server");
+		ipServer.setRequired(true);
+		options.addOption(ipServer);
+		
+		Option service = new Option("s", "service", true, "name of the service for sending");
+		service.setRequired(true);
+		options.addOption(service);
+
+		/* to read the arguments */
+		CommandLineParser parser = new DefaultParser();
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLine cmd;
+
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+			formatter.printHelp("utility-name", options);
+
+			System.exit(1);
+			return;
+		}
+		
+		System.out.println("Publisher is started...");
+
+
+		/* Parse the arguments to the beans constructor */
+		setIpServer(cmd.getOptionValue("ipServer"));
+		setService(cmd.getOptionValue("service"));
+		
+
+		/* Launch the Spring-boot application */
+		new SpringApplicationBuilder(PublisherApplication.class).web(false).run(args);
+	}
+}
---
+++ microServices/src/main/java/com/withBoardManager/receiver/Runner.java
@@ -0,0 +1,124 @@
+package com.withBoardManager.receiver;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+//import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	private final RabbitTemplate rabbitTemplate;
+	public static String serviceForSending = "";
+
+	public static void setServiceForSending(String service) {
+		serviceForSending = service;
+	}
+
+	@SuppressWarnings("unused")
+	private final SpringReceiver reciever;
+	private final ConfigurableApplicationContext context;
+
+	public Runner(SpringReceiver receiver, RabbitTemplate rabbitTemplate, ConfigurableApplicationContext context) {
+		this.reciever = receiver;
+		this.rabbitTemplate = rabbitTemplate;
+		this.context = context;
+	}
+	/*
+	 * To generate a random int between two values.
+	 * 
+	 */
+	private int RandomInteger(int aStart, int aEnd, Random aRandom) {
+		if (aStart > aEnd) {
+			throw new IllegalArgumentException("Start cannot exceed End.");
+		}
+		// get the range, casting to long to avoid overflow problems
+		long range = (long) aEnd - (long) aStart + 1;
+		// compute a fraction of the range, 0 <= frac < range
+		long fraction = (long) (range * aRandom.nextDouble());
+		int randomNumber = (int) (fraction + aStart);
+		return randomNumber;
+	}
+
+	public void run(String... args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+
+		/* For the Receiver Mode */
+		if (context.getBean("isReciever").equals(true)) {
+			System.out.println("---- Ready to recieve messages ----");
+			System.out.println("To quit, enter <QUIT>");
+			while (true) {
+				TimeUnit.MILLISECONDS.sleep(100);
+				String message = sc.nextLine();
+				if (message.equals("QUIT"))
+					break;
+			}
+
+		} else {
+			/* For the Publisher Mode */
+			/* if a path to a file is given */
+
+			if (!context.getBean("pathToFile").equals("")) {
+				/* Infinite loop with random wait to simulate activity */
+				int start = (int) context.getBean("START");
+				int end = (int) context.getBean("END");
+				Random random = new Random();
+				while (true) {
+					// System.out.println("Sending a file");
+					String pathToFile = (String) context.getBean("pathToFile");
+					Path path = Paths.get(pathToFile);
+
+					@SuppressWarnings("unchecked")
+					ArrayList<String> serviceToSent = (ArrayList<String>) context.getBean("serviceToSent");
+
+					for (int i = 0; i < serviceToSent.size(); i++) {
+						setServiceForSending(serviceToSent.get(i));
+						System.out.println("Sending a file for the service: " + serviceToSent.get(i));
+						/* Send line by line */
+						Files.lines(path, StandardCharsets.UTF_8).forEachOrdered(
+								l -> rabbitTemplate.convertAndSend("spring-boot-exchanger", serviceForSending, l));
+					}
+					/* for simulate the input */
+					TimeUnit.SECONDS.sleep(RandomInteger(start, end, random));
+				}
+
+			} else {
+				/* Not used anymore*/
+				while (true) {
+					System.out.println("--> Enter the message you want to send:");
+					System.out.println("<enter QUIT to exit the application>");
+					String message = sc.nextLine();
+
+					if (message.equals("QUIT"))
+						break;
+
+					String serviceToSent = "";
+					while (serviceToSent.equals("")) {
+						System.out.println("--> Enter the name of the service you want to send the message:");
+						serviceToSent = sc.nextLine();
+					}
+
+					System.out.println("Sending message...");
+					rabbitTemplate.convertAndSend(serviceToSent, message);
+
+				}
+			}
+		}
+		context.close();
+		sc.close();
+
+	}
+
+}
