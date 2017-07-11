6f6e0e8afebc37e7a06191ed67cfb21d17b2412f
Charles CAPORALI <capolrik@minet.net>
2017-07-11 09:56:44 +0200

    implements the standard interface for the board

---
+++ ReceiverApp/src/main/java/com/withBoardManager/receiver/HomeController.java
@@ -0,0 +1,17 @@
+package com.withBoardManager.receiver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+/**
+    @RequestMapping("/")
+    public String home() {
+        //model.addAttribute("name", name);
+        return "index";
+    }
+**/
+}
