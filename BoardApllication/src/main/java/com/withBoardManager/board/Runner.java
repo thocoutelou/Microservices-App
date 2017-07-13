24839d5b14b68b94bf4f73cf9f79e660fa2a622e
Charles CAPORALI <capolrik@minet.net>
2017-07-13 15:52:20 +0200

    new directory for the BoardApp

---
+++ BoardApllication/src/main/java/com/withBoardManager/board/Runner.java
@@ -0,0 +1,35 @@
+package com.withBoardManager.board;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	@SuppressWarnings("unused")
+	private final RabbitTemplate rabbitTemplate;
+
+	@SuppressWarnings("unused")
+	private final SpringReceiver reciever;
+
+	public Runner(SpringReceiver receiver, RabbitTemplate rabbitTemplate) {
+		this.reciever = receiver;
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+	public void run(String... args) throws Exception {
+
+		while(true) {
+		/* For the Receiver Mode */
+		System.out.println("---- Ready to recieve messages from the Manager ----");
+		while (true) {
+			TimeUnit.MILLISECONDS.sleep(100);
+			
+			}
+		}
+	}
+
+}
