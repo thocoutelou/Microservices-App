9aeada7a6bba9061bea607e444bc0e02f3063ef2
Charles CAPORALI <capolrik@minet.net>
2017-07-19 14:18:50 +0200

    fix the no response bug of the WeCounter

--- ServiceManagerApp/src/main/java/com/withBoardManager/boardManager/SpringReceiver.java
+++ ServiceManagerApp/src/main/java/com/withBoardManager/boardManager/SpringReceiver.java
@@ -24,43 +24,40 @@ public class SpringReceiver {
 
 		System.out.println("Received < " + message + " >");
 
-		// TODO Change the port
 		String result = HTTPrequest.getHTML("http://" + urlToRead + ":8088/" + message);
 		try {
-		JSONObject json = new JSONObject(result);
-		System.out.println("The Web Counting server has sent: " + json.toString());
-		if (json.getBoolean("acall")) {
-			json.remove("acall");
-			/* For the Tcall & boards */
-			if (!ServiceManagerApplication.serviceToSent.contains(json.get("service"))){
-				/*There are no ticket for this service*/
-				return "{\"error\":\" The service is not avaliable\" }";
+			JSONObject json = new JSONObject(result);
+			System.out.println("The Web Counting server has sent: " + json.toString());
+			if (json.getBoolean("acall")) {
+				json.remove("acall");
+				/* For the Tcall & boards */
+				if (!ServiceManagerApplication.serviceToSent.contains(json.get("service"))) {
+					/* There are no ticket for this service */
+					return "{\"error\":\" The service is not avaliable\" }";
+				}
+				((HttpResponse) context.getBean("httpResponse")).setResponse((String) json.get("service"));
+
+				((HttpResponse) context.getBean("httpResponse")).setCount((int) json.get("ticketCalled"));
+				/* Launch the http Response */
+				((HttpResponse) context.getBean("httpResponse")).setNew(true);
+				json.put("id", Runner.compactAnswer(json.getInt("ticketCalled"), json.get("service").toString(),
+						Runner.getServicedManaged().indexOf(json.get("service"))));
+				json.remove("ticketCalled");
+				System.out.println(">>> Sending to TcallApp...");
+				return json.toString();
+			} else {
+				/* For the Kiosk */
+				json.remove("acall");
+				json.put("id", Runner.compactAnswer(json.getInt("ticketNumber"), json.get("service").toString(),
+						Runner.getServicedManaged().indexOf(json.get("service"))));
+				json.remove("ticketNumber");
+				System.out.println(">>> Sending to EmitterApp (kiosk) ...");
+				System.out.println("The ticket:" + json.toString());
+				return json.toString();
 			}
-			((HttpResponse) context.getBean("httpResponse")).setResponse((String) json.get("service"));
-			
-			((HttpResponse) context.getBean("httpResponse")).setCount((int) json.get("ticketCalled"));
-			/*Launch the http Response*/
-			((HttpResponse) context.getBean("httpResponse")).setNew(true);
-			json.put("id",
-					Runner.compactAnswer(json.getInt("ticketCalled"), json.get("service").toString(),
-							Runner.getServicedManaged().indexOf(json.get("service"))));
-			json.remove("ticketCalled");
-			System.out.println(">>> Sending to TcallApp...");
-			return json.toString();
-		} else {
-			/* For the Kiosk */
-			json.remove("acall");
-			json.put("id",
-					Runner.compactAnswer(json.getInt("ticketNumber"), json.get("service").toString(),
-							Runner.getServicedManaged().indexOf(json.get("service"))));
-			json.remove("ticketNumber");
-			System.out.println(">>> Sending to EmitterApp (kiosk) ...");
-			System.out.println("The ticket:" +json.toString());
-			return json.toString();
+		} catch (Exception e) {
+			return ("Error from the ServiceManager: type=Internal Server Error, status=500");
 		}
-	} catch (Exception e) {
-		return("Error from the ServiceManager: type=Internal Server Error, status=500");
-	}
 
 	}
 
