6debd5ecfc73a75ee7b957d76aae76ce58b55c31
Charles CAPORALI <capolrik@minet.net>
2017-07-26 10:36:05 +0200

    good log configuration for TcallApp

--- TcallApp/src/main/java/com/step5/tcall/TcallReceiver.java
+++ TcallApp/src/main/java/com/step5/tcall/TcallReceiver.java
@@ -2,6 +2,11 @@ package com.step5.tcall;
 
 import java.util.concurrent.CountDownLatch;
 
+
+import static com.step5.tcall.Log.LOG_ON;
+import static com.step5.tcall.Log.COMM;
+
+
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
@@ -14,13 +19,17 @@ private CountDownLatch latch = new CountDownLatch(1);
     
 
     public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
+    	if (LOG_ON && COMM.isInfoEnabled()) 
+			COMM.info("RECEIVED: "+message);
+        //System.out.println("Received <" + message + ">");
         try {
             JSONObject json =new JSONObject(message);
             CallController.setJsonResponse(json);
         }
         catch (Exception e) {
-			System.out.println("Not a valide JSON object.");
+        	if (LOG_ON && COMM.isInfoEnabled()) 
+        		COMM.error("ERROR: Not a valid JSON object");
+			//System.out.println("Not a valid JSON object.");
 		}
         latch.countDown();
     }
